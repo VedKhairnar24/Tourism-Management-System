b41a2dd23f6239da200c3d2e11203a2f2d123db7
VedKhairnar24 <vedkhairnar4@gmail.com>
2025-06-18 01:54:44 +0530

    add all files

---
+++ tms/includes/header.php
@@ -0,0 +1,95 @@
+<?php if($_SESSION['login'])
+{?>
+<div class="top-header bg-gradient-to-r from-blue-600 to-blue-800 text-white py-2">
+	<div class="container">
+		<ul class="tp-hd-lft wow fadeInLeft animated flex items-center space-x-4" data-wow-delay=".5s">
+			<li class="hover:text-blue-200 transition-colors"><a href="index.html" class="flex items-center"><i class="fa fa-home mr-1"></i> Home</a></li>
+			<li class="hover:text-blue-200 transition-colors"><a href="profile.php">My Profile</a></li>
+			<li class="hover:text-blue-200 transition-colors"><a href="change-password.php">Change Password</a></li>
+			<li class="hover:text-blue-200 transition-colors"><a href="tour-history.php">My Tour History</a></li>
+			<li class="hover:text-blue-200 transition-colors"><a href="issuetickets.php">Raised Tickets</a></li>
+		</ul>
+		<ul class="tp-hd-rgt wow fadeInRight animated flex items-center space-x-4" data-wow-delay=".5s"> 
+			<li>Welcome :</li>				
+			<li class="font-semibold"><?php echo htmlentities($_SESSION['login']);?></li> 
+			<li><a href="logout.php" class="hover:text-blue-200 transition-colors">/ Logout</a></li>
+		</ul>
+		<div class="clearfix"></div>
+	</div>
+</div><?php } else {?>
+<div class="top-header bg-gradient-to-r from-blue-600 to-blue-800 text-white py-2">
+	<div class="container">
+		<ul class="tp-hd-lft wow fadeInLeft animated flex items-center space-x-4" data-wow-delay=".5s">
+			<li class="hover:text-blue-200 transition-colors"><a href="index.php" class="flex items-center"><i class="fa fa-home mr-1"></i> Home</a></li>
+			<li class="hover:text-blue-200 transition-colors"><a href="admin/index.php">Admin Login</a></li>
+		</ul>
+		<ul class="tp-hd-rgt wow fadeInRight animated flex items-center space-x-4" data-wow-delay=".5s"> 
+			<li><i class="fa fa-phone mr-1"></i> Toll Number : 123-4568790</li>				
+			<li><a href="#" data-toggle="modal" data-target="#myModal" class="hover:text-blue-200 transition-colors">Sign Up</a></li> 
+			<li><a href="#" data-toggle="modal" data-target="#myModal4" class="hover:text-blue-200 transition-colors">/ Sign In</a></li>
+		</ul>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<?php }?>
+<!--- /top-header ---->
+<!--- header ---->
+<div class="header bg-white shadow-md py-4">
+	<div class="container">
+		<div class="logo wow fadeInDown animated" data-wow-delay=".5s">
+			<a href="index.php" class="text-3xl font-bold">
+				<span class="text-blue-600">Tourism</span> 
+				<span class="text-gray-600">Management System</span>
+			</a>	
+		</div>
+	
+		<div class="lock fadeInDown animated flex items-center" data-wow-delay=".5s"> 
+			<li class="text-green-600"><i class="fa fa-lock mr-2"></i></li>
+			<li><div class="securetxt text-green-600 font-semibold">SAFE &amp; SECURE</div></li>
+			<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /header ---->
+<!--- footer-btm ---->
+<div class="footer-btm wow fadeInLeft animated bg-gray-50 shadow-inner" data-wow-delay=".5s">
+	<div class="container">
+	<div class="navigation">
+			<nav class="navbar navbar-default">
+				<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+				  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				  </button>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+					<nav class="cl-effect-1">
+						<ul class="nav navbar-nav flex space-x-6">
+							<li><a href="index.php" class="hover:text-blue-600 transition-colors">Home</a></li>
+							<li><a href="page.php?type=aboutus" class="hover:text-blue-600 transition-colors">About</a></li>
+								<li><a href="package-list.php" class="hover:text-blue-600 transition-colors">Tour Packages</a></li>
+								<li><a href="page.php?type=privacy" class="hover:text-blue-600 transition-colors">Privacy Policy</a></li>
+								<li><a href="page.php?type=terms" class="hover:text-blue-600 transition-colors">Terms of Use</a></li>
+								<li><a href="page.php?type=contact" class="hover:text-blue-600 transition-colors">Contact Us</a></li>
+								<?php if($_SESSION['login'])
+{?>
+								<li>Need Help?<a href="#" data-toggle="modal" data-target="#myModal3" class="hover:text-blue-600 transition-colors"> / Write Us </a></li>
+								<?php } else { ?>
+								<li><a href="enquiry.php" class="hover:text-blue-600 transition-colors"> Enquiry </a></li>
+								<?php } ?>
+								<div class="clearfix"></div>
+
+						</ul>
+					</nav>
+				</div><!-- /.navbar-collapse -->	
+			</nav>
+		</div>
+		
+		<div class="clearfix"></div>
+	</div>
+</div>
